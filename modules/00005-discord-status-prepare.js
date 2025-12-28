448eb60b4d35bfd213ca156a919e27db4fb10b02
Bullseye <bullseye@ralfreschke.de>
2025-12-28 23:59:40 +0100

    Fixed issue that discord-status contained json

--- modules/00005-discord-status-prepare.js
+++ modules/00005-discord-status-prepare.js
@@ -96,6 +96,8 @@ export default async function getDiscordStatusPrepareFlow(baseCore) {
   workingObject.Tools = [];
   workingObject.doNotWriteToContext = true;
 
+  workingObject.simplifiedContext = true;
+
   log(
     `prepared status prompt (len=${prompt.length}) for randomly chosen channel ${targetChannel} (pool size=${allowedList.length})`,
     "debug",
